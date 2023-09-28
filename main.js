115ada1685c1c961e8dcda8fed087eef3a02a6a1
Tommy Tho <tommythowm@gmail.com>
2023-09-28 11:51:52 -0700

    created a sphere geometry with point light

--- main.js
+++ main.js
@@ -1,34 +1,44 @@
 import * as THREE from "three";
 import WebGL from "three/addons/capabilities/WebGL.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
+let camera, scene, renderer;
+
+// scene
+scene = new THREE.Scene();
+
+// camera
+camera = new THREE.PerspectiveCamera(
+  45,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
+camera.position.z = 20;
 
-const renderer = new THREE.WebGLRenderer();
+// renderer
+renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-const controls = new OrbitControls(camera, renderer.domElement);
-
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// controls
+const controls = new OrbitControls(camera, renderer.domElement);
 
-camera.position.z = 5;
+// geometry
+const geometry = new THREE.SphereGeometry(5, 64, 64);
+const material = new THREE.MeshStandardMaterial({ color: 0xffff00 });
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+
+// lights
+const light = new THREE.PointLight(0xffffff, 100, 100, 1.7);
+light.position.set(0, 10, 10);
+scene.add(light);
 
 // This function will run on every screen refresh (i.e. 60Hz screens means 60 refreshes a second)
 function animate() {
   requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
 
   controls.update();
   renderer.render(scene, camera);
